3ba47ad5e8477c8b2c1cc05ec10733424221680d
Ariesscode <abigailtop95@yahoo.com>
2025-02-02 20:28:31 -0500

    commit changes

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 let searchButton = document.querySelector('#searchFruit')
 
 
-function searchFruit(){
-
+async function searchFruit() {
     let userInput = document.querySelector('#tracker').value.toLowerCase() //.value is not a function, it is a property
+
+    let url = `https://backend-server-app-08b12f65f3cf.herokuapp.com/api/${userInput}`
     console.log(`${userInput} has been searched!`)
+
+    try {
+        const res = await fetch(url)
+        if (!res.ok) {
+            throw new Error(`${res.status}`)
+
+        }
+        const data = await res.json()
+        console.log(data)
+    } catch(error){
+        console.error(`${error}`)
+    }
+
 }
 searchButton.addEventListener('click', searchFruit)
